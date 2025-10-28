b840ef59c9ea534f680e95bd4b6a38484f45de7e
Yusuf Efe Tasdelen <efe@tasdelen.net>
2025-10-28 17:00:24 +0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
-import store from './src/store';
-import { ReactQueryProvider } from './src/utils/ReactQueryProvider';
-import { LanguageProvider } from './src/utils/LanguageContext';
-import { ThemeProvider } from './src/utils/ThemeContext';
-import { ToastProvider } from './src/utils/ToastContext';
-import RootNavigator from './src/navigation/RootNavigator';
-import { ThemedStatusBar } from './src/components/ThemedStatusBar';
+import React from "react";
+import { Provider } from "react-redux";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import store from "./src/store";
+import { ReactQueryProvider } from "./src/utils/ReactQueryProvider";
+import { LanguageProvider } from "./src/utils/LanguageContext";
+import { ThemeProvider } from "./src/utils/ThemeContext";
+import { ToastProvider } from "./src/utils/ToastContext";
+import RootNavigator from "./src/navigation/RootNavigator";
+import { ThemedStatusBar } from "./src/components/ThemedStatusBar";
 
 export default function App() {
   return (
