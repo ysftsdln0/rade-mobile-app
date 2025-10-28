0bd95164906a823ed04e2320a02fce148eb8d4ea
Yusuf Efe Tasdelen <efe@tasdelen.net>
2025-10-28 14:03:22 +0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,83 +1,86 @@
-const typescriptParser = require('@typescript-eslint/parser');
-const typescriptPlugin = require('@typescript-eslint/eslint-plugin');
-const reactPlugin = require('eslint-plugin-react');
-const reactHooksPlugin = require('eslint-plugin-react-hooks');
-const reactNativePlugin = require('eslint-plugin-react-native');
+const typescriptParser = require("@typescript-eslint/parser");
+const typescriptPlugin = require("@typescript-eslint/eslint-plugin");
+const reactPlugin = require("eslint-plugin-react");
+const reactHooksPlugin = require("eslint-plugin-react-hooks");
+const reactNativePlugin = require("eslint-plugin-react-native");
 
 module.exports = [
   {
     ignores: [
-      'node_modules/**',
-      'backend/**',
-      '.expo/**',
-      'dist/**',
-      'build/**',
-      '*.config.js',
-      'metro.config.js',
+      "node_modules/**",
+      "backend/**",
+      ".expo/**",
+      "dist/**",
+      "build/**",
+      "*.config.js",
+      "metro.config.js",
     ],
   },
   {
-    files: ['**/*.ts', '**/*.tsx'],
+    files: ["**/*.ts", "**/*.tsx"],
     languageOptions: {
       parser: typescriptParser,
       parserOptions: {
         ecmaVersion: 2021,
-        sourceType: 'module',
+        sourceType: "module",
         ecmaFeatures: {
           jsx: true,
         },
       },
       globals: {
-        __DEV__: 'readonly',
-        console: 'readonly',
-        setTimeout: 'readonly',
-        clearTimeout: 'readonly',
-        setInterval: 'readonly',
-        clearInterval: 'readonly',
-        fetch: 'readonly',
-        Promise: 'readonly',
-        FormData: 'readonly',
+        __DEV__: "readonly",
+        console: "readonly",
+        setTimeout: "readonly",
+        clearTimeout: "readonly",
+        setInterval: "readonly",
+        clearInterval: "readonly",
+        fetch: "readonly",
+        Promise: "readonly",
+        FormData: "readonly",
       },
     },
     plugins: {
-      '@typescript-eslint': typescriptPlugin,
-      'react': reactPlugin,
-      'react-hooks': reactHooksPlugin,
-      'react-native': reactNativePlugin,
+      "@typescript-eslint": typescriptPlugin,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      "react-native": reactNativePlugin,
     },
     settings: {
       react: {
-        version: 'detect',
+        version: "detect",
       },
     },
     rules: {
       // TypeScript rules
-      '@typescript-eslint/no-unused-vars': ['warn', { 
-        argsIgnorePattern: '^_',
-        varsIgnorePattern: '^_',
-        destructuredArrayIgnorePattern: '^_'
-      }],
-      '@typescript-eslint/no-explicit-any': 'off', // Too many in codebase - can enable gradually
-      '@typescript-eslint/no-non-null-assertion': 'warn',
-      
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "off", // Too many in codebase - can enable gradually
+      "@typescript-eslint/no-non-null-assertion": "warn",
+
       // React rules
-      'react/react-in-jsx-scope': 'off',
-      'react/prop-types': 'off',
-      'react/display-name': 'off',
-      'react-hooks/rules-of-hooks': 'error',
-      'react-hooks/exhaustive-deps': 'warn',
-      
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+      "react/display-name": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+
       // React Native specific
-      'react-native/no-unused-styles': 'off', // Many false positives with dynamic styles
-      'react-native/split-platform-components': 'warn',
-      'react-native/no-inline-styles': 'off', // Common pattern in RN
-      'react-native/no-color-literals': 'off',
-      'react-native/no-raw-text': 'off',
-      
+      "react-native/no-unused-styles": "off", // Many false positives with dynamic styles
+      "react-native/split-platform-components": "warn",
+      "react-native/no-inline-styles": "off", // Common pattern in RN
+      "react-native/no-color-literals": "off",
+      "react-native/no-raw-text": "off",
+
       // General
-      'no-console': ['warn', { allow: ['warn', 'error'] }],
-      'prefer-const': 'warn',
-      'no-var': 'error',
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "prefer-const": "warn",
+      "no-var": "error",
     },
   },
 ];
